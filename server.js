81c243cb1b6fade2440bccbe607e79171096d086
cidicles <admin@ericjseaton.com>
2019-04-30 21:41:29 -0700

    wiring everything up for superlatives generation and basic hosting

--- server.js
+++ server.js
@@ -5,27 +5,29 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http  = require('http');
 //const https  = require('https'); // SSL
+const path = require('path');
 const fs = require('fs');
 const args = process.argv.slice(2);
 const uuidv1 = require('uuid/v1');
 const puppeteer = require('puppeteer');
-
 const morgan = require('morgan');
 const multer = require('multer');
 const crypto = require('crypto');
+const changeCase = require('change-case')
 
+// Enable Body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Logs
 app.use(morgan('dev'));
 
-
-console.log(crypto.randomBytes(16).toString("hex"));
-console.log(uuidv1());
-
-
-
-// enable cores application wide
+// Enable CORS
 app.use(cors());
+
+// Static Serve
+app.use(express.static(path.join(__dirname, 'superlative')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 // SSL
 /*
@@ -35,75 +37,20 @@ let options = {
 };
 */
 
-
-
-
-
-/*
-const address = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg/1920px-Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg";
-phantom.create().then( (ph) => {
-  ph.createPage().then( (page) => {
-
-
-    page.set('content', '<html><body><p>Hello world</p></body></html>');
-
-    page.open(address).then( (status) =>  {
-      //console.log('Opened page: ' + address + ' Status: ' + status);
-      page.property('viewportSize', { width: 1920, height: 1096} )
-      page.render('test.jpg', {format: 'jpeg', quality: '100'}).then(() => {;
-        console.log('Rendered page.');
-        ph.exit();
-      });
-    });
-    
-  });
-});
-*/
-
-
-
-(async function() {
-
-  const browser = await puppeteer.launch();
-
-  const page = await browser.newPage();
-  await page.setContent(`
-  <html>
-    <body style="background-color: #f00;">
-      <img src="https://via.placeholder.com/150">
-      <p>Hello world</p>
-    </body>
-  </html>
-  `);
-  await page.setViewport({ width: 1920, height: 1096});
-
-  console.log(await page.content());
-  await page.screenshot({path: 'screenshot.png'});
-
-  await browser.close();
-
-}());
-
-
-
-
-
-
-
-
-
-// Connection
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var storage = multer.diskStorage({
+// Upload Handler
+const storage = multer.diskStorage({
   destination: function(req, file, callback) {
-    callback(null, __dirname + '/uploads')
+    const cry = crypto.randomBytes(8).toString("hex");
+    const dir = `${__dirname}/uploads/${cry}`;
+    
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    callback(null, dir);
   },
   filename: function(req, file, callback) {
     const extension = file.originalname.split('.').pop();
-
-    //console.log(crypto.randomBytes(16).toString("hex"));
     const uuid = uuidv1();
 
     callback(null, `${uuid}-${Date.now()}.${extension}`);
@@ -111,24 +58,80 @@ var storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.post('/api/img', upload.single('superlative'), (req, res) => {
+// Render Image 
+async function getScreenshot(content) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  if (!fs.existsSync(content.dir)){
+    fs.mkdirSync(content.dir);
+  }
+
+  await page.setContent(`
+  <html>
+    <head>
+    <link href="http://localhost:1337/style.css" rel="stylesheet" />
+    <head>
+    <body style="background-color: #fff; padding: 0; margin:0; display: flex; align-items: center; justify-content: center;">
+      <div class="photo-frame">
+        <div class="photo-frame__mask">
+          <img class="${content.orientationclass} photo-frame__pic--portrait photo-frame__pic" src="${content.img}" alt="Most likely to">
+        </div>
+        <img class="photo-frame__flame" src="http://localhost:1337/flame.png" alt="Tinder">
+        <h3>Most likely to</h3>
+        <h2>${content.text}</h2>
+      </div>
+    </body>
+  </html>
+  `);// TODO: base domain / protocol
+
+  await page.setViewport({
+    width: 350, 
+    height: 500
+  });
+  await page.content();
+  await page.screenshot({
+    path: content.dest
+  });
+  await browser.close();
+}
+
+// Routes 
+app.post('/api/upload', upload.single('superlative'), (req, res) => {
   if (!req.file) {
     return res.send({
       success: false
     });
 
   } else {
-    console.log(req.file);
-    console.log('file received');
-    return res.send({
-      success: true
-    })
+    const img = `http://localhost:1337/${req.file.destination.split('/').slice(-1).join('/')}/${req.file.filename}`; // TODO: base domain / protocol
+    const uuid = uuidv1();
+    const dir = `${__dirname}/superlative/${uuid}`;
+    const filename = `${changeCase.snakeCase(req.body.text)}.png`;
+    const dest = `${dir}/${filename}`;
+    const url = `http://localhost:1337/${uuid}/${filename}`; // TODO: base domain / protocol
+
+    getScreenshot({
+      img,
+      dest,
+      dir,
+      text: req.body.text,
+      orientationclass: req.body.orientationclass
+    }).then(() => {
+      return res.send({
+        success: true,
+        url
+      });
+    }).catch((e) => {
+      console.error(e);
+      return res.send({
+        success: false,
+        error: e
+      });
+    });
   }
 });
 
-// Routes 
-//const routes = require('./api/routes/routes');
-//routes(app);
 
 app.use(function(req, res) {
   res.status(404).send({error: req.originalUrl + ' not found'})
